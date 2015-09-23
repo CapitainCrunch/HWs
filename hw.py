4c3fec4fbec0c75ddf2674bff78226815235675a
CapitainCrunch <evstrat.bg@mail.ru>
2015-09-23 04:36:37 +0300

    1st
    1 exs done

--- hw.py
+++ hw.py
@@ -9,34 +9,80 @@ f = open('long_poem.txt', 'r', encoding='utf-8')
 in_words = open('words.txt', 'r', encoding='utf-8')
 f_out = open('snippets.txt', 'w', encoding='utf8')
 
-clear_text = re.sub('[.!,-:?")(]', '', f.read())
+#clear_text = re.sub('[.!,-:/?")(]', '', f.read())
 
+lines = []
 num_lines = {}
-for num, line in enumerate(clear_text):
+LAST_LINE = 0
+for num, line in enumerate(f):
     num_lines[num] = line
+    lines.append(line)
+    LAST_LINE += 1
+
+f.close()
+
+def find():
+    f = open('long_poem.txt', 'r', encoding='utf-8')
+    words_set = set()
+    for l in f:
+        l = l.split()
+        for w in l:
+            w = w.strip('.:-…/",)(!?')
+            words_set.add(w)
+    f.close()
 
 
-f = open('long_poem.txt', 'r', encoding='utf-8')
-words_set = set()
-for l in f:
-    l = l.split()
-    for w in l:
-        w = w.strip('.:-/",)(!?')
-        words_set.add(w)
-
-index = {}
-for word in words_set:
-    arr = []
-    print(word)
-    for key, value in num_lines.items():
-        m = re.search('\\b' + word + '\\b', value)
-        if m != None:
-            arr.append(key)
-    index[word] = arr
-for k,v in index.items():
-    print(k,v)
+    index = {}
+    for word in words_set:
+        arr = []
+        for key, value in num_lines.items():
+            m = re.search('(.*?)\s.*?' + word + '.*?', value)
+            if m != None:
+                arr.append(key)
+        index[word.lower()] = arr
+    return index
 
 
+def start():
+    f_out = open('snippets.txt', 'w', encoding='utf8')
+    a = time.time()
+    for in_word in in_words:
+        for k,v in find().items():
+            if in_word == k:
+                for i in v:
+                    if i == LAST_LINE:
+                        f_out.write(lines[i-1])
+                        f_out.write(lines[i])
+                    elif i == 0:
+                        f_out.write(lines[i])
+                        f_out.write(lines[i+1])
+                    else:
+                        f_out.write(lines[i-1])
+                        f_out.write(lines[i])
+                        f_out.write(lines[i+1])
+    b = time.time()
+    print(b-a)
+    f_out.close()
+
+"""Если хватит терпения, то запускай то, что сверху :D"""
+
+def second():
+    f_out = open('snippets.txt', 'w', encoding='utf8')
+    words_in = open('words.txt', 'r', encoding='utf-8')
+    for user_word in words_in:
+        user_word = user_word.strip('\n')
+        user_word = '\\b' + user_word + '\\b'
+        f_in = open('long_poem.txt', 'r', encoding='utf-8')
+        a = time.time()
+        lines = re.findall('.*?\\n.*?' + user_word + '.*?\\n.*?\\n', f_in.read())
+        b = time.time()
+        f_in.close()
+        for i in lines:
+            f_out.write(i)
+    f_out.close()
+second()
+
+"""_______________________________________2________________________"""
 
 
 
